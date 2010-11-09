bc98cbee85047b93329bc8a47e8ad0680f3c1519
Matt Lightner <mlightner@gmail.com>
2010-11-08 23:59:21 -0600

    Initial commit to delegate_to_class.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'delegate_to_class'
+
+class Test::Unit::TestCase
+end
