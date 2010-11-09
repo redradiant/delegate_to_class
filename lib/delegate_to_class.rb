7b05e9a232003cb94e63e842e1e4afe7e1f68c1d
Matt Lightner <mlightner@gmail.com>
2010-11-09 08:11:01 -0600

    Added ability to override delegation receiver.

--- lib/delegate_to_class.rb
+++ lib/delegate_to_class.rb
@@ -1,5 +1,9 @@
 module DelegateToClass
+  def delegation_receiver
+    self.class
+  end
+
   def method_missing(method, *args, &block)
-    self.class.respond_to?(method) ? self.class.send(method, *args, &block) : super(method, *args, &block)
+    delegation_receiver.respond_to?(method) ? delegation_receiver.send(method, *args, &block) : super(method, *args, &block)
   end
 end
